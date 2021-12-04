fe5091b8198c1f4aba43681422444ccc13432591
FerminLasarte <fermin.lasarte@icloud.com>
2021-12-04 13:52:06 -0300

    primer

---
+++ appExa/appExa/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace appExa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string Name = "";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Name = Input.Text;
+            nameLabel.Content = $"hola, {Name.ToString()}";
+        }
+    }
+}
