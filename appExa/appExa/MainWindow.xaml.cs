54ad3b5a2609311dc470ee4c5bcba62c8c4b9ee9
FerminLasarte <fermin.lasarte@icloud.com>
2021-12-05 19:37:30 -0300

    cambios previos sql

--- appExa/appExa/MainWindow.xaml.cs
+++ appExa/appExa/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
 
 namespace appExa
 {
@@ -15,9 +18,53 @@ namespace appExa
         {
             InitializeComponent();
         }
+        
+        // FALTA SERVIDOR SQL
+        private SqlConnection con = new SqlConnection(@"Data Source=(servidor");
+
+        public void Logear(string User, string Contrasena)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Nombre, Tipo_Usuario FROM usuarios WHERE Usuario = @usuario AND Contrasena = @pas, con");
+                cmd.Parameters.AddWithValue("usuario", User);
+                cmd.Parameters.AddWithValue("pas", Contrasena);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if (dt.Rows.Count == 1)
+                {
+                    this.Hide();
+                    if (dt.Rows[0][1].ToString() == "Admin")
+                    {
+                        // ventana ADMIN
+                    }
+                    else if (dt.Rows[0][1].ToString() == "Usuario")
+                    {
+                        // ventana USER
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El usuario y/o la contrasena ingresados son incorrectos");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
 
         private void Boton_OnClick(object sender, RoutedEventArgs e)
         {
+            Logear(this.Usuario.Text, this.PasswordBox.Password);
             if ((Usuario.Text == User) && (Contrasena ==  PasswordBox.Password))
             {
                 NameLabel.Content = $"hola, {Usuario.Text.ToString()}";
