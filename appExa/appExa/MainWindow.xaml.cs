5469317ef03bd1f50f852c824f89fbf110abbe9f
juanmachanes <juanmanuel17102001@gmail.com>
2021-12-05 17:22:46 -0300

    sadasd

--- appExa/appExa/MainWindow.xaml.cs
+++ appExa/appExa/MainWindow.xaml.cs
@@ -7,7 +7,9 @@ namespace appExa
     /// </summary>
     public partial class MainWindow
     {
-        private string Name = "";
+        
+        private const string User = "root";
+        private const string Contrasena = "toor";
         
         public MainWindow()
         {
@@ -16,20 +18,15 @@ namespace appExa
 
         private void Boton_OnClick(object sender, RoutedEventArgs e)
         {
-            Name = Usuario.Text;
-            NameLabel.Content = $"hola, {Name.ToString()}";
+            if ((Usuario.Text == User) && (Contrasena ==  PasswordBox.Password))
+            {
+                NameLabel.Content = $"hola, {Usuario.Text.ToString()}";
+                Ventana1 ventana = new Ventana1();
+                ventana.Show();
+            }
+            
         }
         
-        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            if (PasswordBox.Password == "Password")
-            {
-                StatusText.Text = "'Password' is not allowed as a password.";
-            }
-            else
-            {
-                StatusText.Text = string.Empty;
-            }
-        }
+
     }
 }
---
+++ appExa/appExa/Ventana1.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace appExa
+{
+    public partial class Ventana1 : Window
+    {
+        public Ventana1()
+        {
+            InitializeComponent();
+        }
+    }
+}
