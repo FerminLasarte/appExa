e18539d595fe6f2144689798f32049ee9a73fb2c
SantiHerrero <75678917+SantiHerrero@users.noreply.github.com>
2021-12-06 16:41:12 -0300

    algo

--- appExa/appExa/Compra.cs
+++ appExa/appExa/Compra.cs
@@ -4,13 +4,20 @@ namespace appExa
 {
     public class Compra
     {
-        private int _precio = 0;
+        private int _precio;
         private List<Articulo> _articulos;
 
-        public void agregarArticulo(ref Articulo art)
+        public void agregarArt(ref Articulo art)
         {
             _articulos.Add(art);
             _precio += art.precio;
         }
+
+        public void borrarPos(int pos)
+        {
+            if (pos < _articulos.Count) {
+                _precio -= _articulos[pos].precio;
+                _articulos.RemoveAt(pos);
+            }
     }
 }
