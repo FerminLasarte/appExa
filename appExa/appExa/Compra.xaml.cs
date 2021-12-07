ba38d65b823d8aa5de487a7c0df8cfca4602f9da
tomasbozzini <tomasbozzini76@gmail.com>
2021-12-07 00:59:33 -0300

    Principal

---
+++ appExa/appExa/Compra.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace appExa
+{
+    public partial class Compra : Window
+    {
+        public Compra()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ appExa/appExa/Principal.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+using System.Windows;
+using System;
+using System.Data;
+namespace appExa
+{
+    public partial class Principal : Page
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
