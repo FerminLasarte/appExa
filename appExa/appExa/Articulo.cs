87e53d7d8d7e18e2c838f34677829f8c9dd88ba4
SantiHerrero <takaherrero@gmail.com>
2021-12-06 23:48:06 -0300

    Funciona

--- appExa/appExa/Articulo.cs
+++ appExa/appExa/Articulo.cs
@@ -15,10 +15,25 @@ namespace appExa
             _disponible = true;
             _disponibles = 0;
         }
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string categoria
+        {
+            get { return _categoria; }
+            set { _categoria = value; }
+        }
         public bool disponible
         {
             get { return _disponible; }
             set { _disponible = value; }
+        }
+        public int precio
+        {
+            get { return _precio; }
+            set { _precio = value; }
         }
         public int disponibles
         {
