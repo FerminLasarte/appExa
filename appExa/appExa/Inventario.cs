20defad1dd1f3aa6ca693592dbfdf8a9e8355105
SantiHerrero <takaherrero@gmail.com>
2021-12-06 23:34:40 -0300

    mejoras

--- appExa/appExa/Inventario.cs
+++ appExa/appExa/Inventario.cs
@@ -5,7 +5,20 @@ namespace appExa
     public class Inventario
     {
         private Articulo[] _datos;
+        private int _max = 0;
+        private int _actual = 0;
+        public void setMax(int max)
+        {
+            _datos = new Articulo[max];
+            _max = max;
+        }
+        public void agregar(ref Articulo art)
+        {
+            if (_actual < _max) {
+                _datos[_actual] = art;
+                _actual++;
+            }
+        }
 
-        algo
     }
 }
