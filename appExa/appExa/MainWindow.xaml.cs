a2d1f081e716d29d2e23990dbd1f0937c0ca1451
FerminLasarte <fermin.lasarte@icloud.com>
2021-12-06 21:09:10 -0300

    base de datos login register

--- appExa/appExa/MainWindow.xaml.cs
+++ appExa/appExa/MainWindow.xaml.cs
@@ -18,59 +18,33 @@ namespace appExa
         {
             InitializeComponent();
         }
-        
-        // FALTA SERVIDOR SQL
-        private SqlConnection con = new SqlConnection(@"Data Source=(servidor");
-
-        public void Logear(string User, string Contrasena)
-        {
-            try
-            {
-                con.Open();
-                SqlCommand cmd = new SqlCommand("SELECT Nombre, Tipo_Usuario FROM usuarios WHERE Usuario = @usuario AND Contrasena = @pas, con");
-                cmd.Parameters.AddWithValue("usuario", User);
-                cmd.Parameters.AddWithValue("pas", Contrasena);
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
-
-                if (dt.Rows.Count == 1)
-                {
-                    this.Hide();
-                    if (dt.Rows[0][1].ToString() == "Admin")
-                    {
-                        // ventana ADMIN
-                    }
-                    else if (dt.Rows[0][1].ToString() == "Usuario")
-                    {
-                        // ventana USER
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("El usuario y/o la contrasena ingresados son incorrectos");
-                }
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-                throw;
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
 
         private void Boton_OnClick(object sender, RoutedEventArgs e)
         {
-            Logear(this.Usuario.Text, this.PasswordBox.Password);
-            if ((Usuario.Text == User) && (Contrasena ==  PasswordBox.Password))
+            Registro registro = new Registro();
+            registro.Show();
+        }
+        
+        SqlConnection sqlCon = new SqlConnection("server = DESKTOP-BRILGCD\\SERVIDORAPPEXA; database = BaseEXA; integrated security = true;");
+        
+        private void BotonIngresar_OnClick(object sender, RoutedEventArgs e)
+        {
+            sqlCon.Open();
+            SqlCommand consulta = new SqlCommand("select Usuario, Password from Usuario where Usuario = @tUsuario AND Password = @tPassword", sqlCon);
+            consulta.Parameters.AddWithValue("@tUsuario", Usuario.Text);
+            consulta.Parameters.AddWithValue("@tPassword", PasswordBox.Password);
+            SqlDataReader lector = consulta.ExecuteReader();
+
+            if (lector.Read())
+                MessageBox.Show("Inicio de sesion correcto");
+            else
             {
-                NameLabel.Content = $"hola, {Usuario.Text.ToString()}";
-                Ventana1 ventana = new Ventana1();
-                ventana.Show();
+                MessageBox.Show("El usuario y/o la contrasena son incorrectos");
             }
+<<<<<<< Updated upstream
+=======
+            sqlCon.Close();
+>>>>>>> Stashed changes
         }
     }
 }
---
+++ appExa/appExa/UsuarioDAL.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace appExa
+{
+    public class UsuarioDAL
+    {
+        public static int RegistrarUsuario(string usuario, string contrasena)
+        {
+            int resultado = 0;
+            
+            private SqlConnection con = BDComun
+        }
+    }
+}
