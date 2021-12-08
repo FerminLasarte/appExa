282536ed809ab64702322a9afcf08be3e8fea042
FerminLasarte <fermin.lasarte@icloud.com>
2021-12-08 19:25:52 -0300

    arreglos

--- appExa/appExa/Registro.xaml.cs
+++ appExa/appExa/Registro.xaml.cs
@@ -16,10 +16,11 @@ namespace appExa
         SqlConnection sqlCon = new SqlConnection("server = DESKTOP-BRILGCD\\SERVIDORAPPEXA; database = BaseEXA; integrated security = true;");
         private void BotonRegistrarse_OnClick(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Llega");
             if (this.Contrasena.Password == this.ConfirmContrasena.Password)
             {
                 sqlCon.Open();
-                string consulta = "insert into Usuario values ("+Nombre.Text+", '"+Apellido.Text+"', '"+Email.Text+"', '"+Usuario.Text+"', '"+Contrasena.Password+"')";
+                string consulta = "insert into Usuarios values ("+Nombre.Text+", '"+Apellido.Text+"', '"+Email.Text+"', '"+Usuario.Text+"', '"+Contrasena.Password+"')";
                 SqlCommand comando = new SqlCommand(consulta, sqlCon);
                 comando.ExecuteNonQuery();
                 sqlCon.Close();
